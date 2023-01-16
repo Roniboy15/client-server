d3705b72da49a28871c412300795e07ba267223d
Your Name <you@example.com>
2023-01-16 10:34:56 +0100

    ok

---
+++ client/src/routers/appRouters.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomeAdmin from '../components/admin/homeAdmin';
+import Home from '../components/home';
+import Login from '../components/pages/login';
+import Logout from '../components/pages/logout';
+import SignUp from '../components/pages/signUp';
+import ToDoList from '../components/pages/userCMS/toDoList';
+import LayoutAdmin from '../layout/layoutAdmin/layoutAdmin';
+import Layout from '../layout/layoutUser/layout';
+const AppRouters = () => {
+
+  return (
+    <Router>
+      <Routes>
+    {/* User Layout */}
+        <Route path='/' element={<Layout />}>
+          
+          <Route index element={<Home />} />
+          <Route path='/signUp' element={<SignUp />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/todo' element={<ToDoList />} />
+          <Route path='/logout' element={<Logout />} />
+          
+      
+        </Route>
+
+    {/* Admin Layout */}
+        <Route path='/admin' element={<LayoutAdmin />}>
+          <Route path='/admin' element={<HomeAdmin />} />
+        </Route>
+
+
+{/* Not Found */}
+        <Route path='/*' element={<h1>Not Found 404</h1>} />
+      </Routes>
+
+
+    </Router>
+  )
+}
+
+export default AppRouters
